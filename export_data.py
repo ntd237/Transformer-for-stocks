c09621ba87ca959be3cca0ae2cd048f89f2386ba
ntd237 <nguyentiendung23072003@gmail.com>
2024-09-12 18:51:01 +0700

    update

--- export_data.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-# Đọc dữ liệu từ file CSV
-data = pd.read_csv('AAPL_stock_data.csv')
-
-# Xuất dữ liệu sang file Excel
-data.to_excel('AAPL_stock_data.xlsx', index=False) # index=False để không bao gồm cột index trong file Excel
